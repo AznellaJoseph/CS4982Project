0db58126e0cea820702a9c3032b03c181d729a65
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-01-24 14:02:11 -0500

    Added Connection Class and connectionString
    Added Connection class and connection string to be used when accessing the local database file in the DAL classes.

---
+++ code/CapstoneBackend/DAL/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneBackend.DAL
+{
+    /// <summary>
+    /// Connection Class to connect to local database
+    /// </summary>
+    public class Connection
+    {
+        public static readonly string connectionString = "server=127.0.0.1;;port=3306;uid=root;database=capstone;";
+    }
+}
