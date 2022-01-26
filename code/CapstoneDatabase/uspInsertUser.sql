464ed0f1d0a9e319514b45281245bc967edac597
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-01-26 13:58:56 -0500

    Added Database Project
    Added CapstoneDatabase SQLServer project to solution to locally have a instance of the database rather than using MySQL or any other application outside of VisualStudio.

---
+++ code/CapstoneDatabase/uspInsertUser.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[uspInsertUser]
+	@username VARCHAR(45),
+	@password VARCHAR(45),
+	@fname VARCHAR(45),
+	@lname VARCHAR(45)
+
+AS
+	INSERT INTO [dbo].[user](username, password, fname, lname)
+	VALUES (@username, @password, @fname, @lname)
+RETURN 0
