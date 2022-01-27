3fed6a86c5acdae6c532b422b197d8e9a5db68f5
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-01-26 19:17:11 -0500

    Added Login Window and Minimal ViewModel Implementation
    Added the UI for the Login screen. Binded values to ViewModel and added the Command usage fro button use.

--- code/CapstoneDesktop/ViewModels/MainWindowViewModel.cs
+++ code/CapstoneDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using CapstoneDesktop.Utility;
+using JetBrains.Annotations;
 
 namespace CapstoneDesktop.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+
+        private string _username = string.Empty;
+        private string _password = string.Empty;
+
+        public string Username
+        {
+            get => _username;
+            set => this._username = value;
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => this._password = value;
+        }
+
+        public RelayCommand LoginCommand { get; set; }
+        public RelayCommand CreateAccountCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.LoginCommand = new RelayCommand(this.login, this.canLogin);
+            this.CreateAccountCommand = new RelayCommand(this.createAccount, this.canCreateAccount);
+        }
+
+        private bool canLogin(object obj)
+        {
+            return true;
+        }
+
+        private void login(object obj)
+        {
+
+        }
+
+        private bool canCreateAccount(object obj)
+        {
+            return true;
+        }
+
+        private void createAccount(object obj)
+        {
+
+        }
+
     }
 }
