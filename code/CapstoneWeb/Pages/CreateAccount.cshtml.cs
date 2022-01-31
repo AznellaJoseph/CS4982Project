e4c0e12eadf7731c59b17affb352fd61f52c05a9
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-01-31 09:58:28 -0500

    Added Create Account Form, Added in UserManager Functionality
    Added form to the create account page, linked it to the login page. Added properties to link page to pagemodel, made values required. Added in UserManager functionality for login and create user.

--- code/CapstoneWeb/Pages/CreateAccount.cshtml.cs
+++ code/CapstoneWeb/Pages/CreateAccount.cshtml.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using CapstoneBackend.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CapstoneWeb.Pages
@@ -8,11 +11,33 @@ namespace CapstoneWeb.Pages
     /// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
     public class CreateAccountModel : PageModel
     {
+        public int Id { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
         /// <summary>
         ///     Called when [get].
         /// </summary>
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            var response = UserManager.RegisterUser(Username ?? string.Empty, Password ?? string.Empty, FirstName ?? string.Empty, LastName ?? string.Empty);
+            if (response.StatusCode == 200)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ErrorMessage = response.ErrorMessage ?? "Unknown Error.";
+            }
+
+            return Page();
         }
     }
 }
--- code/CapstoneWeb/Pages/Login.cshtml.cs
+++ code/CapstoneWeb/Pages/Login.cshtml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using CapstoneBackend.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CapstoneWeb.Pages
@@ -9,8 +14,13 @@ namespace CapstoneWeb.Pages
     /// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
     public class LoginModel : PageModel
     {
+        public int Id { get; set; }
+
         public string Username { get; set; }
+
         public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
 
         /// <summary>
         ///     Called when [get].
@@ -22,9 +32,20 @@ namespace CapstoneWeb.Pages
         /// <summary>
         ///     Called when [post].
         /// </summary>
-        public void OnPost()
+        public IActionResult OnPost()
         {
-            Debug.WriteLine("LOGIN");
+            var response = UserManager.GetUserByCredentials(Username ?? string.Empty, Password ?? string.Empty);
+            if (string.IsNullOrEmpty(response.ErrorMessage))
+            {
+                return RedirectToPage("Index");
+            }
+
+            else
+            {
+                ErrorMessage = response.ErrorMessage;
+            }
+
+            return Page();
         }
     }
 }
