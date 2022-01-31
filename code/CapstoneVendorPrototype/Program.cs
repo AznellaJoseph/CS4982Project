218014ea6af42919a232cad63f2b85d902a4816f
Alexander Schmidt <aschmid3@my.westga.edu>
2022-01-31 12:33:06 -0500

    Fixed Error and Added User Input to The Prototype
    Fixed Error in Program.cs of Prototype and add User Input to test the query.

--- code/CapstoneVendorPrototype/Program.cs
+++ code/CapstoneVendorPrototype/Program.cs
@@ -2,15 +2,32 @@
 
 namespace CapstoneVendorPrototype
 {
-    class Program
+    /// <summary>
+    /// A Prototype Program to test Vendor API
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        
+        /// <summary>
+        /// The main entry to test the prototype.
+        /// </summary>
+        /// <param name="args">the args for the execution</param>
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Ender a latitude:");
+            var latitude = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ender a longitude:");
+            var longitude = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ender a radius:");
+            var radius = Convert.ToInt32(Console.ReadLine());
             PlaceClient client = new();
-            var result = client.GetPointsOfInterest(35.8308, -90.7023);
+            var result = client.GetPointsOfInterest(latitude, longitude, radius);
+            Console.WriteLine("Points of Interest:");
             foreach (var aPlace in result.Result.Results)
             {
                 Console.WriteLine(aPlace.Name);
+                Console.WriteLine(aPlace.Location.Address);
+                Console.WriteLine(aPlace.Location.Region);
             }
         }
     }
