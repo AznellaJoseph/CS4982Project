39d3a0b05c4ad89605f151bca1ac4c3e3ed64382
Alexander Schmidt <aschmid3@my.westga.edu>
2022-02-06 13:48:35 -0500

    Changed waypoint.sql to use DATETIME
    Changed waypoint.sql to use DATETIME because it was not following the ERD.

--- code/CapstoneDatabase/waypoint.sql
+++ code/CapstoneDatabase/waypoint.sql
@@ -5,8 +5,8 @@ CREATE TABLE waypoint
     tripId INT UNSIGNED NOT NULL,
     waypointNum INT UNSIGNED NOT NULL,
     location VARCHAR(75) NOT NULL,
-    startDate DATE NOT NULL,
-    endDate DATE NOT NULL,
+    startTime DATETIME NOT NULL,
+    endTime DATETIME NOT NULL,
     PRIMARY KEY (tripId, waypointNum),
     CONSTRAINT fk_waypoint_tripId
         FOREIGN KEY (tripId)
