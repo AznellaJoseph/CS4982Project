103a5e9877d72d22cc46f81db30ad0b5608e5784
dbranch3 <dbranch3@my.westga.edu>
2022-02-07 18:32:45 -0500

    Fixed syntax and bugs with procedure

--- code/CapstoneDatabase/uspCreateWaypoint.sql
+++ code/CapstoneDatabase/uspCreateWaypoint.sql
@@ -2,25 +2,23 @@ DROP PROCEDURE IF EXISTS uspCreateWaypoint;
 
 DELIMITER $
 
-CREATE PROCEDURE uspAddWaypoint(
-    IN tripId int,
-    IN location VARCHAR(75),
-    In startTime DATETIME,
-    In endTime DATETIME,
+CREATE PROCEDURE uspCreateWaypoint(
+    tripId INT,
+    location VARCHAR(75),
+    startTime DATETIME,
+    endTime DATETIME
 )
 BEGIN
+DECLARE numWaypointsInTrip INT;
 
-DECLARE @numWaypointsInTrip int;
-
-SET @numWaypointsInTrip = (
-    SELECT COUNT(*)
-    FROM waypoint
-    WHERE waypoint.tripId = tripId
-) + 1;
+SELECT COUNT(*) + 1
+INTO numWaypointsInTrip
+FROM waypoint
+WHERE waypoint.tripId = tripId;
 
 INSERT INTO waypoint (tripId, waypointNum, location, startTime, endTime)
-VALUES (tripId, @numWaypointsInTrip, location, startTime, endTime);
+VALUES (tripId, numWaypointsInTrip, location, startTime, endTime);
 
-SELECT @numWaypointsInTrip;
+SELECT numWaypointsInTrip AS waypointNum;
 
 END$
