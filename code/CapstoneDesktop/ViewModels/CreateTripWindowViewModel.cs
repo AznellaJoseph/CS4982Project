ff898da36fcd8deb9b334f329e73daf2c40d5a0e
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-02-14 13:55:48 -0500

    Added Starting CreateWaypoint and CreateTrip UI
    Added the starting UI for the create waypoint and create trip functionality. Added bindings for fields necessary for functionality. Updated script to account for if the script is in a folder with spaces.

--- code/CapstoneDesktop/ViewModels/CreateTripWindowViewModel.cs
+++ code/CapstoneDesktop/ViewModels/CreateTripWindowViewModel.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+using CapstoneBackend.Model;
+using ReactiveUI;
 
 namespace CapstoneDesktop.ViewModels
 {
+
+    /// <summary>
+    /// ViewModel for Create Trip Window
+    /// </summary>
+    /// <seealso cref="CapstoneDesktop.ViewModels.ViewModelBase" />
     public class CreateTripWindowViewModel : ViewModelBase
     {
+        private readonly TripManager _tripManager;
+
+        private string _error = string.Empty;
+
+        public string ErrorMessage
+        {
+            get => this._error;
+            set => this.RaiseAndSetIfChanged(ref _error, value);
+        }
+
+        public string? TripName { get; set; }
+        public string? Notes { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public ReactiveCommand<Unit, Unit> CreateTripCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> CancelCreateTripCommand { get; set; }
+
+        public CreateTripWindowViewModel(TripManager manager)
+        {
+            this._tripManager = manager;
+            this.CreateTripCommand = ReactiveCommand.Create(this.createTrip);
+            this.CancelCreateTripCommand = ReactiveCommand.Create(this.cancelCreateTrip);
+        }
+
+        public CreateTripWindowViewModel() : this(new TripManager())
+        {
+
+        }
+
+        private void createTrip()
+        {
+
+        }
+
+        private void cancelCreateTrip()
+        {
+
+        }
+
     }
 }
--- code/CapstoneDesktop/ViewModels/CreateWaypointWindowViewModel.cs
+++ code/CapstoneDesktop/ViewModels/CreateWaypointWindowViewModel.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+using CapstoneBackend.Model;
+using ReactiveUI;
 
 namespace CapstoneDesktop.ViewModels
 {
     public class CreateWaypointWindowViewModel : ViewModelBase
     {
+        private readonly WaypointManager _waypointManager;
+
+        public CreateWaypointWindowViewModel(WaypointManager manager)
+        {
+            this._waypointManager = manager;
+            this.CreateWaypointCommand = ReactiveCommand.Create(this.createWaypoint);
+            this.CancelCreateWaypointCommand = ReactiveCommand.Create(this.cancelCreateWaypoint);
+        }
+
+        public CreateWaypointWindowViewModel() : this(new WaypointManager())
+        {
+        }
+
+        private string _error = string.Empty;
+
+        public string ErrorMessage
+        {
+            get => this._error;
+            set => this.RaiseAndSetIfChanged(ref this._error, value);
+        }
+
+        public DateTime StartDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+        public string? WaypointLocation { get; set; }
+        public string? Notes { get; set; }
+
+        public ReactiveCommand<Unit, Unit> CreateWaypointCommand { get; set; }
+        public ReactiveCommand<Unit, Unit> CancelCreateWaypointCommand { get; set; }
+
+        private void createWaypoint()
+        {
+
+        }
+
+        private void cancelCreateWaypoint()
+        {
+
+        }
     }
 }
