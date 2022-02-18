70dc8f96ef1274f82d9a87594077f0e2e22d9d7c
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-02-18 04:25:26 -0500

    Added LandingPage and MainWindow ViewModel Tests
    Added more ViewModel tests to increase viewmodel code coverage. Coverage is now 62% for the viewmodel package.

---
+++ code/CapstoneTest/DesktopTests/ViewModels/TestLandingPage/TestLoadTrips.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using CapstoneBackend.Model;
+using CapstoneDesktop.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ReactiveUI;
+
+namespace CapstoneTest.DesktopTests.ViewModels.TestLandingPage
+{
+    [TestClass]
+    public class TestLoadTrips
+    {
+        [TestMethod]
+        public void LoadTrips_NotNullResponse_AddsOneTrip()
+        {
+            var mockUser = new Mock<User>
+            {
+                Object =
+                {
+                    UserId = 0
+                }
+            };
+            var mockTripManager = new Mock<TripManager>();
+            var mockScreen = new Mock<IScreen>();
+
+            mockTripManager.Setup(tm => tm.GetTripsByUser(0))
+                .Returns(new Response<IList<Trip>> {Data = new List<Trip> {new()}});
+
+            LandingPageViewModel landingPageViewModel = new(mockUser.Object, mockTripManager.Object, mockScreen.Object);
+
+            Assert.AreEqual(1, landingPageViewModel.TripViewModels.Count);
+        }
+
+        [TestMethod]
+        public void LoadTrips_NullResponse_EmptyList()
+        {
+            var mockUser = new Mock<User>
+            {
+                Object =
+                {
+                    UserId = 0
+                }
+            };
+            var mockTripManager = new Mock<TripManager>();
+            var mockScreen = new Mock<IScreen>();
+
+            mockTripManager.Setup(tm => tm.GetTripsByUser(0)).Returns(new Response<IList<Trip>> {Data = null});
+
+            LandingPageViewModel landingPageViewModel = new(mockUser.Object, mockTripManager.Object, mockScreen.Object);
+
+            Assert.AreEqual(0, landingPageViewModel.TripViewModels.Count);
+        }
+    }
+}
