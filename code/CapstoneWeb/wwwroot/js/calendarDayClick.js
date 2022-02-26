53bd2b2ac38b73ef730596c724aaa6da19fbf302
Alexander Schmidt <aschmid3@my.westga.edu>
2022-02-26 12:02:03 -0500

    Added Ajax to display waypoints on calendar click.
    Added Ajax to display a list of waypoint with out reloading the page.

---
+++ code/CapstoneWeb/wwwroot/js/calendarDayClick.js
@@ -0,0 +1,42 @@
+$(".vanilla-calendar-date").click(
+    function () {
+        let pad = function(num) { return ('00'+num).slice(-2) };
+        let date = new Date($(this).attr("data-calendar-date"));
+        date = date.getUTCFullYear()         + '-' +
+            pad(date.getUTCMonth() + 1)  + '-' +
+            pad(date.getUTCDate())
+        
+        let tripId = parseInt($("#tripId").attr("value"))
+        $.ajax({
+            method: "GET",
+            url:"/trip/?handler=Ajax",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("XSRF-TOKEN",
+                    $('input:hidden[name="__RequestVerificationToken"]').val());
+            },
+            data: { tripId: tripId, selectedDate: date },
+            success: function (result) {
+                result.data.forEach(function(waypoint) {
+                    console.log(waypoint)
+                    let element = document.createElement("p");
+                    element.innerHTML = waypoint.location;
+                    let events = $("#events")
+                    events.empty();
+                    events.append(
+                        `
+                        <div>
+                            <p>
+                                ${waypoint.startDate} - ${waypoint.startDate}
+                            </p>
+                            <p>
+                                ${waypoint.location}
+                            </p>
+                            <a>Remove</a>
+                        </div>
+                        `
+                    );
+                })
+            }
+        })
+    }
+)
