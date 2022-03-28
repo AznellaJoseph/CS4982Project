8951e81d25bf98cae7ac77290345b23857820a97
Alexander Schmidt <aschmid3@my.westga.edu>
2022-03-27 22:26:30 -0400

    Added tests to lodging dal.
    Added tests to lodging dal, but it has issues with linux so pushing to check on windows.

---
+++ code/CapstoneTest/BackendTests/DAL/TestLodgingDAL/TestCreateLodging.cs
@@ -0,0 +1,58 @@
+﻿using CapstoneBackend.DAL;
+using CapstoneBackend.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace CapstoneTest.BackendTests.DAL.TestLodgingDAL
+{
+    [TestClass]
+    public class TestCreateLodging
+    {
+        private MySqlConnection _connection;
+        private int testTripId;
+        private int testLodgingId;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _connection = new MySqlConnection(Connection.ConnectionString);
+            testTripId = new TripDal(_connection).CreateTrip(1, "TestTrip", "Some Notes", DateTime.Now, DateTime.Now);
+        }
+
+        [TestMethod]
+        public void CallProcedure_WithInvalidTripId_Fails()
+        {
+            LodgingDal testDAL = new(_connection);
+
+            Assert.ThrowsException<MySqlException>(() => testDAL.CreateLodging(-1, "TestLocation", DateTime.Now, DateTime.Now, "Some Notes"));
+        }
+
+        [TestMethod]
+        public void CallProcedure_WithValidInput_Succeeds()
+        {
+            LodgingDal testDAL = new(_connection);
+
+            int? resultID = testDAL.CreateLodging(testTripId, "TestLocation", DateTime.Now, DateTime.Now, "Some Notes");
+            testLodgingId = (int) resultID;
+
+            Assert.IsTrue(resultID.HasValue);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _connection.Open();
+            string removeTrip = $"delete from trip where tripId = {testTripId};";
+            string removeLodging = $"delete from lodging where lodgingId = {testLodgingId};";
+
+            using MySqlCommand tripCmd = new MySqlCommand(removeTrip, _connection);
+            tripCmd.ExecuteNonQuery();
+
+            using MySqlCommand lodgingCmd = new MySqlCommand(removeLodging, _connection);
+            lodgingCmd.ExecuteNonQuery();
+
+            this._connection.Close();
+        }
+    }
+}
---
+++ code/CapstoneTest/BackendTests/DAL/TestLodgingDAL/TestRemoveLodging.cs
@@ -0,0 +1,60 @@
+﻿using CapstoneBackend.DAL;
+using CapstoneBackend.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace CapstoneTest.BackendTests.DAL.TestLodgingDAL
+{
+    [TestClass]
+    public class TestRemoveLodging
+    {
+        private MySqlConnection _connection;
+        private int testTripId;
+        private int testLodgingId;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _connection = new MySqlConnection(Connection.ConnectionString);
+            testTripId = new TripDal(_connection).CreateTrip(1, "TestTrip", "Some Notes", DateTime.Now, DateTime.Now);
+        }
+
+        [TestMethod]
+        public void CallProcedure_WithInvalidLodgingId_ReturnsFalse()
+        {
+            LodgingDal testDAL = new(_connection);
+
+            bool result = testDAL.RemoveLodging(-1);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CallProcedure_WithValidLodgingId_ReturnsTrue()
+        {
+            LodgingDal testDAL = new(_connection);
+            testLodgingId = testDAL.CreateLodging(1, "TestLocation", DateTime.Now, DateTime.Now, "Some Notes");
+
+            bool result = testDAL.RemoveLodging(testLodgingId);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _connection.Open();
+            string removeTrip = $"delete from trip where tripId = {testTripId};";
+            string removeLodging = $"delete from lodging where lodgingId = {testLodgingId};";
+
+            using MySqlCommand tripCmd = new MySqlCommand(removeTrip, _connection);
+            tripCmd.ExecuteNonQuery();
+
+            using MySqlCommand lodgingCmd = new MySqlCommand(removeLodging, _connection);
+            lodgingCmd.ExecuteNonQuery();
+
+            this._connection.Close();
+        }
+    }
+}
