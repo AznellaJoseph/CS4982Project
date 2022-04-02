ecd81f5ead0b0faece513eb89c0d98f3f2703c90
dbranch3 <dbranch3@my.westga.edu>
2022-04-02 16:52:53 -0400

    Added callback when clicking an autocomplete result
    Callback currently shows the name of the place clicked inside of the map element in the HTML. This can be expanded to load an actual map in the future.

--- code/CapstoneWeb/wwwroot/js/googleMaps.js
+++ code/CapstoneWeb/wwwroot/js/googleMaps.js
@@ -1,6 +1,9 @@
 ﻿
 let autocomplete;
 
+/**
+ * Initializes the autocomplete on an input element with  the ID: 'location'
+ * */
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
         document.getElementById('location'),
@@ -8,4 +11,19 @@ function initAutocomplete() {
             types: ['establishment'],
             fields: ['place_id', 'geometry', 'name']
         });
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+/**
+ * Callback for when an autocomplete option is clicked/changed
+ * */
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+        document.getElementById('location').placeholder = 'Enter a place:';
+    } else {
+        document.getElementById('map').innerHTML = place.name;
+    }
 }
