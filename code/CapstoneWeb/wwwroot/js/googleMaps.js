16a12ce3733da162a6e7d52b01602a93d13661cb
dbranch3 <dbranch3@my.westga.edu>
2022-04-02 16:40:10 -0400

    Integrated Google Maps JavaScriptSDK for Autocomplete Waypoint Locations
    The SDK was brought into the projects and locations now autocomplete when typing in the location field of creating waypoints.

---
+++ code/CapstoneWeb/wwwroot/js/googleMaps.js
@@ -0,0 +1,11 @@
+﻿
+let autocomplete;
+
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('location'),
+        {
+            types: ['establishment'],
+            fields: ['place_id', 'geometry', 'name']
+        });
+}
