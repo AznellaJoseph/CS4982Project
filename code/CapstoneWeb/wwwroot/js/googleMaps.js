c7dac7b1d3ccb8c4ce87462e7b2b939923d2282f
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-04-02 21:28:09 -0400

    Removed Logs to Console, Implemented Code Suggestions
    Removed the unneeded logs to the console that were used to debug the method. Implemented some code suggestions to clean up the code and make better use of the javascript language.

--- code/CapstoneWeb/wwwroot/js/googleMaps.js
+++ code/CapstoneWeb/wwwroot/js/googleMaps.js
@@ -6,38 +6,37 @@ let autocomplete;
  * */
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('location'),
+        document.getElementById("location"),
         {
-            types: ['establishment'],
-            fields: ['place_id', 'geometry', 'name']
+            types: ["establishment"],
+            fields: ["place_id", "geometry", "name"]
         });
 
-    autocomplete.addListener('place_changed', onPlaceChanged);
+    autocomplete.addListener("place_changed", onPlaceChanged);
 }
 
 /**
  * Callback for when an autocomplete option is clicked/changed
  * */
 function onPlaceChanged() {
-    var place = autocomplete.getPlace();
+    const place = autocomplete.getPlace();
 
     if (!place.geometry) {
-        document.getElementById('location').placeholder = 'Enter a place:';
+        document.getElementById("location").placeholder = "Enter a place:";
     } else {
-        const map = new google.maps.Map(document.getElementById('map'), {zoom: 20, center: place.geometry.location });
+        const map = new google.maps.Map(document.getElementById("map"), {zoom: 20, center: place.geometry.location });
         const marker = new google.maps.Marker({ position: place.geometry.location, map: map });
     }
 }
 
 function initMap() {
-    console.log("initMap");
-    var address = document.getElementById("location").innerHTML;
+    const address = document.getElementById("location").innerHTML;
     const geocoder = new google.maps.Geocoder();
     
     geocoder.geocode({ 'address': address },
         function(results, status) {
             if (status == google.maps.GeocoderStatus.OK && status != google.maps.GeocoderStatus.ZERO_RESULTS) {
-                const map = new google.maps.Map(document.getElementById('map'),
+                const map = new google.maps.Map(document.getElementById("map"),
                     { zoom: 20, center: results[0].geometry.location });
                 const marker = new google.maps.Marker({ position: results[0].geometry.location, map: map });
             }
