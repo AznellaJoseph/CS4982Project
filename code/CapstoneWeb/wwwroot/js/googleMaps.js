482cc349a3b078325ba0171539039e6424855c82
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-04-02 20:28:48 -0400

    Started Map View for Trip Item Site
    Started using the google maps api to show the map of the lodging and waypoint on their respective sites.

--- code/CapstoneWeb/wwwroot/js/googleMaps.js
+++ code/CapstoneWeb/wwwroot/js/googleMaps.js
@@ -9,7 +9,7 @@ function initAutocomplete() {
         document.getElementById('location'),
         {
             types: ['establishment'],
-            fields: ['place_id', 'geometry', 'name', 'formatted_address']
+            fields: ['place_id', 'geometry', 'name']
         });
 
     autocomplete.addListener('place_changed', onPlaceChanged);
@@ -24,7 +24,25 @@ function onPlaceChanged() {
     if (!place.geometry) {
         document.getElementById('location').placeholder = 'Enter a place:';
     } else {
-        const map = new google.maps.Map(document.getElementById('map'), {zoom: 14, center: place.geometry.location });
+        const map = new google.maps.Map(document.getElementById('map'), {zoom: 20, center: place.geometry.location });
         const marker = new google.maps.Marker({ position: place.geometry.location, map: map });
     }
+}
+
+function initMap() {
+    console.log("initMap");
+    const location = document.getElementById("location").text;
+    const geocoder = new google.maps.Geocoder();
+    
+    geocoder.geocode({ 'address': location },
+        function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK && status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+                console.log(results[0].geometry.location);
+                const map = new google.maps.Map(document.getElementById('map'),
+                    { zoom: 20, center: results[0].geometry.location });
+                const marker = new google.maps.Marker({ position: results[0].geometry.location, map: map });
+            } else {
+                console.log(status);
+            }
+        });
 }
