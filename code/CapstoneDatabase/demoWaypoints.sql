7eed337edf382c0abd3176cc295e55a992d1eded
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-04-04 10:03:12 -0400

    Added Demo Lodgings
    Added demo lodgings used for demo purposes. Added new waypoint to other trip. Modified CSS to present the event location when on a singular waypoint or lodging site so it doesn't take as much space.

--- code/CapstoneDatabase/demoWaypoints.sql
+++ code/CapstoneDatabase/demoWaypoints.sql
@@ -1,7 +1,2 @@
-CALL uspCreateWaypoint(
-    1,
-    "demo location",
-    "demo notes",
-    "2022-02-18",
-    "2022-02-19"
-);
+CALL uspCreateWaypoint(1, "Starbucks, South Park Street, Carrollton, GA, USA", "Bring reusable cup.", "2022-02-18 08:00:00", "2022-02-18 09:00:00");
+CALL uspCreateWaypoint(2, "Starbucks, South Park Street, Carrollton, GA, USA", "Use the app for points", "2022-02-26 08:00:00", "2022-02-26 09:00:00");
