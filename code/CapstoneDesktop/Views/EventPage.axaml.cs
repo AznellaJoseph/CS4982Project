c73dfff89422c19c7747c5646006554aae4f8d04
Alexander Schmidt <aschmid3@my.westga.edu>
2022-04-04 12:36:44 -0400

    Added GMap.
    Added Gmap and Added to Event Page. It is not working testing if it is a linux issue.

--- code/CapstoneDesktop/Views/EventPage.axaml.cs
+++ code/CapstoneDesktop/Views/EventPage.axaml.cs
@@ -1,6 +1,10 @@
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using CapstoneDesktop.ViewModels;
+using GMap.NET;
+using GMap.NET.Avalonia;
+using GMap.NET.MapProviders;
 using ReactiveUI;
 
 namespace CapstoneDesktop.Views
@@ -8,15 +12,24 @@ namespace CapstoneDesktop.Views
     /// <summary>
     ///     User Control for the Event Page Functionality
     /// </summary>
-    /// <seealso cref="Avalonia.ReactiveUI.ReactiveUserControl&lt;CapstoneDesktop.ViewModels.EventPageViewModel&gt;" />
+    /// <seealso cref="EventPageViewModel" />
     public class EventPage : ReactiveUserControl<EventPageViewModel>
     {
+
+        public GMapControl MainMap { get; set; }
         /// <summary>
         ///     Initializes a new instance of the <see cref="EventPage" /> class.
         /// </summary>
         public EventPage()
         {
             InitializeComponent();
+            
+            GoogleMapProvider.Instance.ApiKey = "AIzaSyARhel5-jZFkChP1uASkhk0G7qYc5cRiWA";
+
+            MainMap = this.Get<GMapControl>("GMap");
+            MainMap.MapProvider = GMapProviders.GoogleMap;
+            MainMap.Position = new PointLatLng(44.4268, 26.1025);
+            MainMap.FillEmptyTiles = true;
         }
 
         private void InitializeComponent()
