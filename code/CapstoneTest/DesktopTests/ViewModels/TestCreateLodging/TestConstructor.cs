a55a8bd691d9de2984d093d767af26e5dd08f273
dbranch3 <dbranch3@my.westga.edu>
2022-04-05 20:08:05 -0400

    Wrote tests for the Autocomplete method in the GooglePlacesService
    Also modified constructor tests for CreateLodging and CreateWaypoint to accomodate changes needed for autocomplete.

--- code/CapstoneTest/DesktopTests/ViewModels/TestCreateLodging/TestConstructor.cs
+++ code/CapstoneTest/DesktopTests/ViewModels/TestCreateLodging/TestConstructor.cs
@@ -18,17 +18,18 @@ namespace CapstoneTest.DesktopTests.ViewModels.TestCreateLodging
 
             Assert.AreEqual(string.Empty, createLodgingViewModel.ErrorMessage);
             Assert.AreEqual(mockScreen.Object, createLodgingViewModel.HostScreen);
+            Assert.AreEqual(string.Empty, createLodgingViewModel.Location);
             Assert.IsNotNull(createLodgingViewModel.UrlPathSegment);
             Assert.IsNotNull(createLodgingViewModel.CreateLodgingCommand);
             Assert.IsNotNull(createLodgingViewModel.CancelCreateLodgingCommand);
             Assert.IsNotNull(createLodgingViewModel.ValidationManager);
             Assert.IsNotNull(createLodgingViewModel.LodgingManager);
+            Assert.IsNotNull(createLodgingViewModel.AutocompletePredictions);
             Assert.IsNull(createLodgingViewModel.StartTime);
             Assert.IsNull(createLodgingViewModel.EndTime);
             Assert.IsNull(
                 createLodgingViewModel.StartDate);
             Assert.IsNull(createLodgingViewModel.EndDate);
-            Assert.IsNull(createLodgingViewModel.Location);
             Assert.IsNull(
                 createLodgingViewModel.Notes);
         }
