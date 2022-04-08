fff9c49ceb80472d3eb8f2ad49b402173bcbcb90
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-04-08 00:06:05 -0400

    Added More Demo Objects to Database
    Added demo lodgings and modified demo transportation to provide a more accurate example of what those objects in the database could look like.

--- code/CapstoneDatabase/demoLodgings.sql
+++ code/CapstoneDatabase/demoLodgings.sql
@@ -1,1 +1,2 @@
 CALL uspCreateLodging(1, "Hilton Atlanta, Courtland Street Northeast, Atlanta, GA, USA", "Bring extra blankets", "2022-02-15", "2022-02-20");
+CALL uspCreateLodging(2, "Courtyard by Marriott Carrollton, Barnes Avenue, Carrollton, GA, USA", "", "2022-02-25", "2022-03-04");
