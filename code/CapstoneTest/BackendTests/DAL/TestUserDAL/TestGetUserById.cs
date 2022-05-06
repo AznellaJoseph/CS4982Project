bbd065ef9c71d4923ab28bd95012ada9fa9f179f
Aznella Joseph <71835603+AznellaJoseph@users.noreply.github.com>
2022-05-06 10:00:29 -0400

    Added Coverage Report, Updated Tests
    Added a coverage report for "sprint 5" to show the test coverage of the final state of the system. Updated tests to increase coverage of new functionality and added methods and fields.

---
+++ code/CapstoneTest/BackendTests/DAL/TestUserDAL/TestGetUserById.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapstoneBackend.DAL;
+using CapstoneBackend.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+
+namespace CapstoneTest.BackendTests.DAL.TestUserDAL
+{
+    [TestClass]
+    public class TestGetUserById
+    {
+        private readonly MySqlConnection _connection = new(Connection.ConnectionString);
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            DeleteTestUser();
+        }
+
+        [TestMethod]
+        public void CallProcedure_InvalidId_ReturnsNull()
+        {
+            UserDal testDal = new(_connection);
+
+            var result = testDal.GetUserById(-1);
+
+            Assert.IsTrue(result is null);
+        }
+
+        [TestMethod]
+        public void CallProcedure_ValidId_ReturnsUser()
+        {
+            InsertTestUser();
+            UserDal testDal = new(_connection);
+
+            var result = testDal.GetUserById(1);
+
+            Assert.IsTrue(result is not null);
+            Assert.IsInstanceOfType(result, typeof(User));
+        }
+
+        private void InsertTestUser()
+        {
+            _connection.Open();
+            const string query = "INSERT user (username, password, fname, lname) " +
+                                 "VALUES ('TestUsername', 'TestPassword', 'TestFirstName', 'TestLastName');";
+
+            using var cmd = new MySqlCommand(query, _connection);
+            cmd.ExecuteNonQuery();
+            _connection.Close();
+        }
+
+        private void DeleteTestUser()
+        {
+            _connection.Open();
+            const string query = "delete from user where username = 'TestUsername';";
+
+            using var cmd = new MySqlCommand(query, _connection);
+            cmd.ExecuteNonQuery();
+            _connection.Close();
+        }
+    }
+}
